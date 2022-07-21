60a3a7018af5501e7019cf1e8399c172ef065025
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2022-07-21 22:54:22 +0300

    Update 621-task-scheduler.py

--- 621-task-scheduler/621-task-scheduler.py
+++ 621-task-scheduler/621-task-scheduler.py
@@ -1,6 +1,7 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        output, h = 0, []
+        output = 0
+        h = []
         
         c = Counter(tasks)
         for k,v in c.items():
@@ -14,7 +15,8 @@ class Solution:
                     nums,key = heapq.heappop(h)
                     nums += 1
                     if nums < 0: temp.append((nums,key))
-                if not h and not temp: break
+                if not h and not temp: 
+                    break
                 i += 1
             for k,v in temp:
                 heapq.heappush(h,(k, v))
