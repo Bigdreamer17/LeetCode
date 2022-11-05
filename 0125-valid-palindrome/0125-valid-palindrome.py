a6b4132ab5e3dae8943c498c45b9c1ae26648290
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2022-11-05 15:40:55 +0300

    Time: 53 ms (89.62%), Space: 14.4 MB (85.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,17 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # Intiating Two pointers one at the front(l) and one at the back(r)
         l = 0 
         r = len(s) - 1
         
+        
         while l < r:
+            # Check if s[l] or s[l] is either alphabet or number if not move pointers
             if not s[l].isalnum():
                 l += 1
             elif not s[r].isalnum():
                 r -= 1
                 
             else:
+                #Check the lower part  of s[l] or s[r] are not equal if so return False  
                 if s[l].lower() != s[r].lower():
                     return False
+                # Otherwise Increment l, decrement r and return True
                 else:
                     l += 1
                     r -= 1 
