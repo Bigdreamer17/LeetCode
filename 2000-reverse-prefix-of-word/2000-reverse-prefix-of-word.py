914ca3b93f03215d66d26fff9b761b6aaa416181
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2022-11-05 15:01:56 +0300

    Time: 61 ms (30.86%), Space: 13.8 MB (97.15%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -1,7 +1,11 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
+        # Looping through the range of the string
         for i in range(len(word)):
+            # Check if our current character is equal to ch
             if word[i] == ch:
+                # If so return the reversed characters plus the characters after it 
                 return word[:i+1][::-1] + word[i+1:]
+        # Else if you cab't find the ch character return the word itself
         return word 
         
