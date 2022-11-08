60700e6697f8738fd63fc0995fb07fe803dbdb7d
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2022-11-08 13:56:22 +0300

    Time: 52 ms (74.37%), Space: 13.9 MB (20.78%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
@@ -1,12 +1,17 @@
 class Solution:
     def smallestSubsequence(self, s: str) -> str:
+        # A dictionary to hold our chars last position 
         l = {}
         for i in range(len(s)):
             l[s[i]] = i
+        # create a stack to track our ans and a set to check which once we will be looking for
         stack = []
         v  = set()
+        
         for i in range(len(s)):
+            #Loop through s and if s[i] is not in stack add it to both stack and set 
             if s[i] not in stack:
+                #if it is and stack is not empty and the last item of stack is > s[i] and the value of stack[-1] in our dict is > i pop from both stack and set 
                 while(stack and stack[-1] > s[i] and l[stack[-1]] > i):
                     v.remove(stack.pop())
                 
