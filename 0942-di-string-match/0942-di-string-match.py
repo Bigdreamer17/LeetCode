8b5d4bc1e4970d26fce86040ea343f1d82c8a740
Estifanos F/mariam <60184109+Bigdreamer17@users.noreply.github.com>
2022-11-10 15:42:00 +0300

    Time: 158 ms (24.33%), Space: 15.2 MB (47.01%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.py
@@ -0,0 +1,13 @@
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        ans = []
+        l, h = 0, len(s)
+       
+        for i in s:
+            if i == 'I':
+                ans.append(l)
+                l += 1
+            else:
+                ans.append(h)
+                h -= 1
+        return ans + [l]
